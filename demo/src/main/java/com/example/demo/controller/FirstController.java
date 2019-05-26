64aed938a307e3a9513c0ae6504d9299d9fbe5aa
nampe <nampe@DESKTOP-133A2EF>
2019-05-26 12:47:42 +0530

    controller class added

---
+++ demo/src/main/java/com/example/demo/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+
+public class FirstController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		User u = new User();
+		u.setUid(123);
+		u.setUname("sagar");
+		System.out.println(u);
+	}
+
+}
